63cf40288475a16c52cb4b5860ab9d146fab247c
Marlond <mcriollo@mit.edu>
2023-08-25 10:07:33 -0700

    Added Files

---
+++ app/Contract.tsx
@@ -0,0 +1,103 @@
+import React, { useRef } from 'react';
+
+const Contract = ({ data }) => {
+  const clauseCounter = useRef(1);
+
+    interface Block {
+    type: string;
+    children: any;
+    bold?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+    parent?: any;
+  }
+  let parent = null;
+
+  const renderBlock = (block:Block) => {
+
+    switch (block.type) {
+      case 'block':
+        return (
+          <div className='whitespace-pre'>
+            {block.children.map(renderBlock)}
+          </div>
+        );
+      case 'p':
+        return<div className ='whitespace-pre'>
+
+        <p>{block.children.map(renderElement)}</p>
+          <br></br>
+        </div>
+      case 'clause':
+        let clauseNumber = clauseCounter.current++;
+
+        return (
+          <div className="whitespace-pre ">
+            {clauseNumber } {block.children.map(renderBlock)}
+          <br></br>
+          </div>
+        );
+
+      case 'li':
+          return block.children.map(renderBlock);
+      case 'ul':
+            return block.children.map(renderBlock);
+  
+      case 'lic':
+          return (
+              <li>
+                  <span className="clause-number">
+
+                  {block.children.map(renderElement)}
+                  </span>
+              </li>
+          );
+      case 'h1':
+        return <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{block.children.map(renderElement)}</h1>;
+        
+      
+      default:
+                return <div>{block.children.map(renderElement)}</div>;
+
+    }
+  };
+
+
+
+  const renderElement = (element) => {
+    if (element.type === 'mention') {
+      return (
+        <span
+          key={element.id}
+          style={{ backgroundColor: element.color }}
+          title={element.title}
+        >
+          {element.children[0].text}
+        </span>
+      );
+      
+    } else if (element.underline) {
+      return <u>{element.text}</u>;
+
+    } else if (element.bold) {
+      return <strong>{element.text}</strong>;
+    } else if (element.italic) {
+      return <em>{element.text}</em>;
+    }
+    else if (element.type === 'clause') {
+      return (
+        <div className="whitespace-pre ">
+          <br></br>
+          {element.children.map(renderBlock)}
+        </div>
+      );
+    }
+    else {
+      return element.text;
+    }
+  };
+
+  return <div>{data.map(renderBlock)}</div>;
+};
+
+export default Contract;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
-import Image from 'next/image'
+// 'use client';
+// import React, { useState, useEffect } from 'react';
+// import inputData from './input.json';
 
-export default function Home() {
+// import './App.css'; // We'll add styles in this file
+
+// let clauseCounter = 1; // global clause counter
+
+
+
+// function Block({ data }: {data: any}) {
+//   if (!data || !data.type) return null;
+//   console.log(data);
+//   switch (data.type) {
+//     case 'h1':
+//       return <h1>{data.children.map((child: any) => <Text key={child.text} data={child} />)}</h1>;
+//     case 'p':
+//       return <p>{data.children.map((child: any) => <Text key={child.text} data={child} />)}</p>;
+//     case 'block':
+//       return <Block key={child.text} data={child} />
+// ;
+//     case 'clause':
+//       return <Clause data={data} />;
+//     case 'ul':
+//       return (
+//         <ul>
+//           {data.children.map((child, index) => (
+//             <li key={index}>{child.children.map(c => <Text key={c.text} data={c} />)}</li>
+//           ))}
+//         </ul>
+//       );
+//     default:
+//             return null;
+  
+//   }
+// }
+
+// function Text({ data }) {
+//   if (data.bold) return <strong>{data.text}</strong>;
+//   if (data.type === 'mention') return <Mention data={data} />;
+//   return data.text;
+// }
+
+// function Clause({ data }) {
+//   return (
+//     <div className="clause">
+//       <h4>
+//         {clauseCounter++}. {data.title}
+//       </h4>
+//       {data.children.map((child, index) => (
+//         <Block key={index} data={child} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function Mention({ data }) {
+//   return (
+//     <span className="mention" style={{ backgroundColor: data.color }}>
+//       {data.children[0].text}
+//     </span>
+//   );
+// }
+
+// function App() {
+//   const [documentData, setDocumentData] = useState([]);
+//   useEffect(() => {
+//     // Fetch the data from the local JSON file
+//     fetch("/input.json")
+//       .then((response) => response.json())
+//       .then((documentData) => {
+//         // Set the data into state
+//         setDocumentData(documentData);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching the JSON data:", error);
+//       });
+//   }, []);
+//   console.log(documentData);
+
+//   return (
+//     <div className="App">
+
+//       <h1>Document</h1>
+
+//       {documentData.map((block, index) => (
+
+//         <Block key={index} data={block} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ``
+'use client';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Contract from './Contract.tsx';
+
+
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/input.json')
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(jsonData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching the JSON file:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="App">
+      <Contract data={data} />
+    </div>
+  );
 }
+
+
+export default App;
